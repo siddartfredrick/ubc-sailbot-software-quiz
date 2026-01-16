4ba0a97b7146f46b26e4b871832fec746236f8f7
Siddart Fredrick <35365853+siddartfredrick@users.noreply.github.com>
2026-01-15 23:29:11 -0800

    Implement angle utilities and add unit tests

--- python/standard_calc.py
+++ python/standard_calc.py
@@ -1,17 +1,18 @@
 def bound_to_180(angle):
-    """Bounds the provided angle between [-180, 180) degrees.
 
-    e.g.)
-        bound_to_180(135) = 135.0
-        bound_to_180(200) = -160.0
-
-    Args:
-        angle (float): The input angle in degrees.
-
-    Returns:
-        float: The bounded angle in degrees.
     """
-    return 0
+    Bounds the provided angle between [-180, 180) degrees.
+
+    We first add 180° to shift the angle into a 0–360° coordinate system.
+    Then we take modulo 360 to find where that angle lies on the unit circle.
+    Finally, we subtract 180° to shift back into the −180° to +180° coordinate system.
+
+    """
+
+    wrapped = (angle + 180) % 360
+    wrapped = wrapped - 180
+
+    return wrapped 
 
 
 def is_angle_between(first_angle, middle_angle, second_angle):
@@ -29,4 +30,18 @@ def is_angle_between(first_angle, middle_angle, second_angle):
     Returns:
         bool: True when `middle_angle` is not in the reflex angle of `first_angle` and `second_angle`, false otherwise.
     """
-    return True
+
+    a = first_angle % 360
+    b = middle_angle % 360
+    c = second_angle % 360
+
+    def CCW_dist(x):
+        return (x-a) % 360
+    
+    dist_ac = CCW_dist(c)
+    dist_ab = CCW_dist(b)
+
+    if dist_ac <= 180:
+        return dist_ab <= dist_ac
+
+    return dist_ab >= dist_ac
--- python/test_standard_calc.py
+++ python/test_standard_calc.py
@@ -7,9 +7,44 @@ from standard_calc import bound_to_180, is_angle_between
 def test_bound_basic1():
     assert bound_to_180(0) == 0
 
+def test_bound_wrap_positive():
+    assert bound_to_180(360) == 0
+    assert bound_to_180(720) == 0
+
+
+def test_bound_wrap_negative():
+    assert bound_to_180(-360) == 0
+    assert bound_to_180(-720) == 0
+
+
+def test_bound_edges():
+    assert bound_to_180(180) == -180
+    assert bound_to_180(-180) == -180
+
+
+def test_bound_general():
+    assert bound_to_180(200) == -160
+    assert bound_to_180(-190) == 170
+
 
 """ Tests for is_angle_between() """
 
 
 def test_between_basic1():
     assert is_angle_between(0, 1, 2)
+
+def test_between_simple_false():
+    assert not is_angle_between(0, 3, 2)
+
+
+def test_between_wraparound_true():
+    assert is_angle_between(350, 0, 10)
+
+
+def test_between_wraparound_false():
+    assert not is_angle_between(350, 180, 10)
+
+
+def test_between_reflex_case():
+    assert not is_angle_between(45, 90, 270)
+
